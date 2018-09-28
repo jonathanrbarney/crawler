33159f76a3c3dad4dff934935043ac4eb0e3a1f5
jonathanrbarney <jonathanrbarney@gmail.com>
2018-09-27 22:49:42 -0700

    Initial Commit

---
+++ wpspider/pipelines.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from scrapy import log
+import sqlite3 as sqlite
+
+# This pipeline takes the Item and stuffs it into scrapedata.db
+class domainPipeline(object):
+    def __init__(self):
+        # Possible we should be doing this in spider_open instead, but okay
+        self.connection = sqlite.connect('./wpdomains.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS domains ' \
+                    '(id INTEGER PRIMARY KEY, domain VARCHAR(80), wpcheck INTEGER)')
+
+    # Check for duplicates and if not add table to db.
+    def process_item(self, item, spider):
+
+        domain = item['domain']
+
+        self.cursor.execute("select * from domains where domain=?", (domain,))
+        result = self.cursor.fetchone()
+        if result:
+            log.msg("Domain already in database: %s" % item, level=log.DEBUG)
+        else:
+            self.cursor.execute(
+                "insert into domains (domain, wpcheck) values (?, ?)",
+                    (domain, 2))
+
+            self.connection.commit()
+
+            log.msg(f'Domain Stored: {domain}', level=log.DEBUG)
+        return item
---
+++ wpspider/spiders/domainSpider.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from wpspider.items import Url
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+
+class DomainspiderSpider(scrapy.Spider):
+    name = 'domainSpider'
+    ignore_urls = ['facebook.com','tumblr.com','youtube.com', 'baidu.com','twitter.com','wordpress.com']
+    start_urls = ['https://blogforarizona.net/']
+
+    def parse(self, response):
+        hxs = scrapy.Selector(response)
+        # extract all links from page
+        all_links = hxs.xpath('*//a/@href').extract()
+        # iterate over links
+
+        spltAr = response.url.split("://")
+        i = (0,1)[len(spltAr)>1]
+        domain = spltAr[i].split("?")[0].split('/')[0].split(':')[0].lower()
+
+        for link in LinkExtractor(allow=(), deny=self.ignore_urls.append(domain)).extract_links(response):
+
+            spltAr = link.url.split("://")
+            i = (0, 1)[len(spltAr) > 1]
+            dmn = spltAr[i].split("?")[0].split('/')[0].split(':')[0].lower()
+
+            item = Url()
+            item['domain'] = dmn
+            yield item
+            yield scrapy.http.Request(url=link.url)
